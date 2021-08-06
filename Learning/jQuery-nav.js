bb9e026feffdb02b845e04c562a9298078bbe013
MegumiDavid <dvdshinjo@gmail.com>
2021-08-06 18:09:44 -0300

    progressing

---
+++ Learning/jQuery-nav.js
@@ -0,0 +1,11 @@
+// when the documents is ready -> functions
+$(document).ready(function(){
+    $('.burger-menu').on('click', function(){
+        $('.mobile-view').toggle("fast")
+    })
+}
+)
+
+// toggle -> inverte o CSS (.mobile-view)
+
+// jQuery = $
