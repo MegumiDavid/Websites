735c46bfed1eadda0911efdca8912c9dbd0451e4
David Megumi Shinjo <dvdshinjo@gmail.com>
2022-04-04 16:12:32 -0300

    Update

--- Project 6 - Drum/index.js
+++ Project 6 - Drum/index.js
@@ -26,17 +26,17 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
+      var tom1 = new Audio("sounds/crash.mp3");
       tom1.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
+      var tom2 = new Audio("sounds/kick-bass.mp3");
       tom2.play();
       break;
 
     case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
+      var tom3 = new Audio('sounds/snare.mp3');
       tom3.play();
       break;
 
@@ -46,17 +46,17 @@ function makeSound(key) {
       break;
 
     case "j":
-      var snare = new Audio('sounds/snare.mp3');
+      var snare = new Audio('sounds/tom-3.mp3');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/crash.mp3');
+      var crash = new Audio('sounds/tom-1.mp3');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/kick-bass.mp3');
+      var kick = new Audio('sounds/tom-2.mp3');
       kick.play();
       break;
 
